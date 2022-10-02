2f7e7f34948b34ce867810845805da0ffe5c1caa
Tommaso Bona <piergeolo@gmail.com>
2022-10-02 14:41:17 +0200

    Create T-MUS.py

---
+++ T-MUS.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+import requests
+import urllib
+import json
+import time
+import os
+os.system("toilet T-MUS")
+url = input("Insert URL to scan: ")
+encoded_url = urllib.parse.quote(url, safe='')
+api_url = "https://ipqualityscore.com/api/json/url/6iP3Ci09b83suqDijoQ4pHSD9uayadBp/"
+data = requests.get(api_url + encoded_url)
+print(json.dumps(data.json(), indent=4))
